36ec41be455d4cbfbd49f98d39982340add06ce5
Anthony Borushko <twinpixxx2005@gmail.com>
2019-05-29 22:23:53 +0300

    feat: table generation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,22 @@
-console.log('\'Allo \'Allo!');
-
+$(document).ready(function() {
+	let url = 'https://api.github.com/repos/thomasdavis/backbonetutorials/contributors';
+	$.getJSON(url, function(data) {
+			function generateTable(table, data) {
+				let headers = document.querySelectorAll('th');
+				let keys = [];
+				Array.prototype.forEach.call(headers, function(header) {
+					keys.push(header.dataset.key)
+				});
+			  	for (let element of data) {
+			    	let row = table.insertRow();
+			    	for (let key of keys) {
+			      		let cell = row.insertCell();
+			      		let text = document.createTextNode(element[key]);
+			      		cell.appendChild(text);
+			    }
+			  }
+			}
+			let table = document.querySelector("table");
+			generateTable(table, data);
+	});
+});
